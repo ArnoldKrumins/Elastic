2e66818dab952098dec43663ae6c9ec622fbd745
Arnold Krumins <arnold.krumins@gmail.com>
2015-03-29 22:35:14 +0200

    Initial commit

---
+++ app/module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by arnoldkrumins on 29/03/15.
+ */
+var app = angular.module('elastic',[]);
---
+++ app/services/MobileFriendlyService.js
@@ -0,0 +1,33 @@
+/**
+ * Created by arnoldkrumins on 29/03/15.
+ */
+app.factory('MobileFriendlyService',function($http,$q,$log){
+
+
+    var baseUrl ='https://www.googleapis.com/pagespeedonline/v3beta1/mobileReady?key=AIzaSyDm_EokJZ-ke7LP183GSUgBH5JAgX3qYsY&screenshot=true&snapshots=true&locale=en_US&url=';
+    var postfix ="&strategy=mobile&filter_third_party_resources=false&callback=JSON_CALLBACK";
+
+    return{
+
+        apipath:  baseUrl + callback,
+
+        GetSiteData: function (url) {
+
+            var url = baseUrl + url + postfix;
+
+            var deferred = $q.defer();
+            $http.jsonp(url).success(function (data) {
+                deferred.resolve(data);
+            }).error(function (data, status, headers, config) {
+                $log.info(data);
+                $log.info(status);
+                $log.info(headers);
+                $log.info(config);
+                deferred.reject("an error occured while calling Service");
+            });
+
+            return deferred.promise;
+        }
+    }
+
+});

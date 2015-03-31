02eb8d6fb476ee312b67a5afe827bf07316598cb
Arnold Krumins <arnold.krumins@gmail.com>
2015-03-31 23:00:39 +0200

    further development

--- app/directories/siteStats.js
+++ app/directories/siteStats.js
@@ -6,15 +6,28 @@ app.directive('siteStats',function(){
     return{
           restrict:'EA',
           scope:{ result:'='},
-          template:'<div class="site-stats" ng-hide="result === NULL">' +
-                        '{{ angular.isUndefined(result) }}' +
-                        '<p>score : {{ result.ruleGroups.USABILITY.score }}</p>'+
-                        '<p><i class="fa" ng-class="{\'fa-thumbs-up\':result.ruleGroups.USABILITY.pass,\'fa-thumbs-down\':!result.ruleGroups.USABILITY.pass}"></i></p>'+
+          template:'<div class="site-stats">' +
+                        '<p><i class="fa" ng-class="{\'fa-thumbs-up\':result.ruleGroups.USABILITY.pass,\'fa-thumbs-down\':!result.ruleGroups.USABILITY.pass}"></i><span>{{ result.ruleGroups.USABILITY.score }}%</span></p>'+
+//                        '<img  src="{{ \'data:image/jpeg;base64,\' + result.screenshot.data }}" >' +
                     '</div>',
           link:function(scope,element,__){
 
+            scope.$watch('result',function(){
 
-              var l = scope.result;
+                if(!angular.isUndefined(scope.result))
+                {
+                    var t = new TimelineMax();
+
+                   t.to(element,0.1,{autoAlpha:1});
+                   t.to(element.find('i'), 0.1, {scaleX:0.8, scaleY:0.8, force3D:true, yoyo:true, repeat:2, ease:Power1.easeInOut});
+                    t.play();
+                }
+                else
+                {
+                    TweenMax.to(element, 0, {autoAlpha:0});
+                }
+            })
+
           }
     }
 

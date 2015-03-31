b05289beff15c66c9e18960b08fc3dbfd7a13931
Arnold Krumins <arnold.krumins@gmail.com>
2015-03-31 19:09:01 +0200

    further development

--- app/directories/siteStats.js
+++ app/directories/siteStats.js
@@ -6,14 +6,15 @@ app.directive('siteStats',function(){
     return{
           restrict:'EA',
           scope:{ result:'='},
-          template:'<div>' +
+          template:'<div class="site-stats" ng-hide="result === NULL">' +
+                        '{{ angular.isUndefined(result) }}' +
                         '<p>score : {{ result.ruleGroups.USABILITY.score }}</p>'+
-                        '<p>pass :{{ result.ruleGroups.USABILITY.pass }}</p>'+
+                        '<p><i class="fa" ng-class="{\'fa-thumbs-up\':result.ruleGroups.USABILITY.pass,\'fa-thumbs-down\':!result.ruleGroups.USABILITY.pass}"></i></p>'+
                     '</div>',
           link:function(scope,element,__){
 
-            var s = scope.result;
 
+              var l = scope.result;
           }
     }
 
